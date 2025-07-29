549b8f6fffb103fb6a69e03027bc227b3bf62a57
wlbjunior <wlbjunior@gmail.com>
2025-07-28 23:13:20 -0300

    Create filosofia.py

---
+++ filosofia.py
@@ -0,0 +1,24 @@
+timeline_data = [
+    {
+        "periodo": "Filosofia Antiga",
+        "anos": "c. 600 a.C. - 500 d.C.",
+        "descricao": "Período que abrange desde os pré-socráticos até o fim do Império Romano, com foco na busca pela verdade e na natureza do ser.",
+        "filosofos": [
+            "Tales de Mileto",
+            "Sócrates",
+            "Platão",
+            "Aristóteles"
+        ]
+    },
+    {
+        "periodo": "Filosofia Medieval",
+        "anos": "c. 500 d.C. - 1500 d.C.",
+        "descricao": "Marcada pela influência do cristianismo, islamismo e judaísmo, buscando conciliar fé e razão.",
+        "filosofos": [
+            "Santo Agostinho",
+            "São Tomás de Aquino",
+            "Avicena",
+            "Maimônides"
+        ]
+    }
+]
